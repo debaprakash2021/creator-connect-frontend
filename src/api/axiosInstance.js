5e5329b532b35052020616b83789c25a2d0d49b3
Debaprakash Jena <debaprakashjena2021@gmail.com>
2026-02-24 13:14:27 +0530

    authentication issue resolved

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,21 +1,18 @@
 import axios from "axios";
 import { errorToast } from "../utils/toast";
-import { loginUser } from "./authApi";
+
 const axiosInstance = axios.create({
   baseURL: "https://creator-connect-backend.onrender.com/api",
   withCredentials: true
 });
 
-
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      errorToast("Unauthorized request")
-      // loginUser();
+      errorToast("Session expired. Please log in again.");
       console.log("Unauthorized request");
     }
-
     return Promise.reject(error);
   }
 );
